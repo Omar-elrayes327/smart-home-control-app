eb23153f1f759cca567da2e0d3b86dbfe84efe5d
Omar-elrayes327 <omarelrayes3600@gmail.com>
2026-02-22 14:47:25 +0200

    Update Automation_and_senses.dart

---
+++ lib/screens/Automation_and_senses.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AutomationAndSenses extends StatelessWidget {
+  const AutomationAndSenses({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
